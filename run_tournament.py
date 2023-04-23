734d6d826472a7800e859884c42ac59b0f8a304e
Marina Elmore <melmore@mba2023.hbs.edu>
2023-04-23 17:24:10 -0400

    Structure ready for Rohin

--- run_tournament.py
+++ run_tournament.py
@@ -3,33 +3,41 @@ from random import randrange
 from config import *
 
 
-class Team:
+class Employee:
     def __init__(self, strategy):
         self.strategy = strategy
+        self.payoff = 0
+
+class Team:
+    def __init__(self):
         self.resources = 0
         self.head_count = 0
+        self.employees = []
 
 class Organization:
     def __init__(self):
         self.resources = float(randrange(COMPANY_RESOURCES_MIN, COMPANY_RESOURCES_MAX))
         self.head_count = randrange(COMPANY_HEADCOUNT_MIN, COMPANY_HEADCOUNT_MAX)
+        self.num_teams = NUM_TEAMS
         self.teams = []
 
-        for strategy in STRATEGIES:
-            self.teams.append(Team(strategy))
+        for i in range(0, self.num_teams):
+            self.teams.append(self.allocate_team_resources())        
 
-        self.allocate_team_resources()
-
-    #TODO
+    #TODO Right now it just does it equally between teams
     def allocate_team_resources(self):
-        # Some random algorithm to randomly allocate between teams
-        num_teams = len(self.teams)
+        team = Team()
+        team.resources = float(self.resources/self.num_teams)
+        team.head_count = round(self.head_count/self.num_teams)
 
-        # Right now it just does it equally between teams
-        for team in self.teams:
-            team.resources = float(self.resources/num_teams)
-            team.head_count = round(self.head_count/num_teams)
-    
+        for i in range(0, team.head_count):
+            diceroll = randrange(0,len(STRATEGIES))
+            strategy = STRATEGIES[diceroll]
+            team.employees.append(Employee(strategy))
+
+        return team
+
+
     #TODO
     def reallocate_team_resources(self, tournament):
         scores = tournament.scores
@@ -53,16 +61,19 @@ def allocate_company_resources():
     head_count = randrange(COMPANY_HEADCOUNT_MIN, COMPANY_HEADCOUNT_MAX)
     return resources, head_count
 
-def run_match(teams):
-    team1 = teams[0]
-    team2 = teams[1]
+# TODO - create a tournament where each employee on team1 plays each employee on team2
+def run_match(team1, team2):
+    matches_this_round = []
     num_rounds = max(team1.head_count, team2.head_count)
 
-    # Run Match
-    match = axl.Match((team1.strategy, team2.strategy), num_rounds)
-    match.play()
+    #  TODO Run Match
+    # for all employees:
+        #match = axl.Match((employee1.strategy, employee2.strategy), num_rounds)
+        #match.play()
+        #matches_this_round.append(match outcome)
 
-    return match
+    #return all results from the round
+    return matches_this_round
 
 def run_tournament(teams):
     tournament = axl.Tournament(teams)
@@ -74,15 +85,22 @@ def main():
     my_org = Organization()
 
     # Run Tournament
-    all_rounds = []
-    for round in range(0,NUM_ROUNDS):
+    all_matches = []
+    for games in range(0,NUM_ROUNDS):
+        matches_this_round = []
 
-        tournament_results = run_tournament([team.strategy for team in my_org.teams])
+        # Run tourament between teams round robin
+        for i in range(0, len(my_org.teams)-1):
+            for j in range(i+1, len(my_org.teams)):
+                # This will run all matches between employees on a team
+                match_outcome = run_match(my_org.teams[i], my_org.teams[j])
+                matches_this_round.append(match_outcome)
 
-        all_rounds.append(tournament_results)
+            all_matches.append(matches_this_round)
 
         # TODO - Relloacate resources after each round
-        my_org.reallocate_team_resources(tournament_results)
+        #my_org.reallocate_team_resources(matches_this_round)
+
 
     # TODO - once game over, need to do some analysis on results
 
